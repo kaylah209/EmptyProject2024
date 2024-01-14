5c3e6814eebde15eb175c72e5c21c01bbdd1bac4
FriedLongJohns <81837862+FriedLongJohns@users.noreply.github.com>
2024-01-14 14:03:43 -0800

    let it build (correctly)

---
+++ src/main/java/org/carlmontrobotics/Constants.java
@@ -0,0 +1,15 @@
+package org.carlmontrobotics;
+
+public final class Constants {
+    // public static final class Drivetrain {
+    //     public static final double MAX_SPEED_MPS = 2;
+    // }
+    public static final class OI {
+        public static final class Driver {
+            public static final int port = 0;
+        }
+        public static final class Manipulator {
+            public static final int port = 1;
+        }
+    }
+}
--- src/main/java/org/carlmontrobotics/Robot.java
+++ src/main/java/org/carlmontrobotics/Robot.java
@@ -2,15 +2,20 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
+package org.carlmontrobotics;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
-
   private RobotContainer m_robotContainer;
 
   @Override
@@ -22,15 +27,6 @@ public class Robot extends TimedRobot {
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
   }
-
-  @Override
-  public void disabledInit() {}
-
-  @Override
-  public void disabledPeriodic() {}
-
-  @Override
-  public void disabledExit() {}
 
   @Override
   public void autonomousInit() {
@@ -45,9 +41,6 @@ public class Robot extends TimedRobot {
   public void autonomousPeriodic() {}
 
   @Override
-  public void autonomousExit() {}
-
-  @Override
   public void teleopInit() {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
@@ -58,7 +51,10 @@ public class Robot extends TimedRobot {
   public void teleopPeriodic() {}
 
   @Override
-  public void teleopExit() {}
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
 
   @Override
   public void testInit() {
@@ -69,5 +65,8 @@ public class Robot extends TimedRobot {
   public void testPeriodic() {}
 
   @Override
-  public void testExit() {}
+  public void simulationInit() {}
+
+  @Override
+  public void simulationPeriodic() {}
 }
